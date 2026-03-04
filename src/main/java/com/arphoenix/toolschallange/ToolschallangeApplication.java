34fdb44a145277d989384b48b2b79499e889133c
conuaz <lucas.olasz@basisti.com.br>
2026-03-04 18:00:42 -0300

    first commit

---
+++ src/main/java/com/arphoenix/toolschallange/ToolschallangeApplication.java
@@ -0,0 +1,13 @@
+package com.arphoenix.toolschallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolschallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolschallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arphoenix/toolschallange/ToolschallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arphoenix.toolschallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolschallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
