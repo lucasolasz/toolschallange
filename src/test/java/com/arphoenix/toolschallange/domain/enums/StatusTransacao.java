4fd6c0dfcc272c3613e9a713c868c3ea0272f0f5
lucasolasz <olaszlucas072@gmail.com>
2026-03-04 21:48:45 -0300

    chore: Inclusao dos Enums

---
+++ src/test/java/com/arphoenix/toolschallange/domain/enums/StatusTransacao.java
@@ -0,0 +1,6 @@
+package com.arphoenix.toolschallange.domain.enums;
+
+public enum StatusTransacao {
+
+    PENDENTE, AUTORIZADO, NEGADO, CANCELADO
+}
---
+++ src/test/java/com/arphoenix/toolschallange/domain/enums/TipoPagamento.java
@@ -0,0 +1,5 @@
+package com.arphoenix.toolschallange.domain.enums;
+
+public enum TipoPagamento {
+    AVISTA, PARCELADO_LOJA, PARCELADO_EMISSOR
+}
