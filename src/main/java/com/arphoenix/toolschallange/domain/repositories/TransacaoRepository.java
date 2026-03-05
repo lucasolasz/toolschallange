35982bd7bdb8a2896a4792c7d43200f46282bc36
lucasolasz <olaszlucas072@gmail.com>
2026-03-05 00:51:20 -0300

    fix: Altera o tipo de Id da entidade no repository

--- src/main/java/com/arphoenix/toolschallange/domain/repositories/TransacaoRepository.java
+++ src/main/java/com/arphoenix/toolschallange/domain/repositories/TransacaoRepository.java
@@ -1,11 +1,9 @@
 package com.arphoenix.toolschallange.domain.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.arphoenix.toolschallange.domain.entities.Transacao;
 
-public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
+public interface TransacaoRepository extends JpaRepository<Transacao, String> {
 
 }
